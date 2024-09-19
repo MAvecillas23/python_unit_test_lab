a903d9a1bddd1e3cae0b3098e4e5b974e7e6bccd
Michael <email@>
2024-09-18 20:18:41 -0500

    Finished Lab 4- price_discount & test_discount

--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -1,6 +1,8 @@
 import unittest 
 from unittest import TestCase
-from price_discount import discount  
+from price_discount import discount
+from python_unit_test_lab.cart_discount import price_discount
+
 
 class TestDiscount(TestCase):
 
@@ -10,7 +12,39 @@ class TestDiscount(TestCase):
         self.assertEqual(expected_discount, discount(prices))
 
     
-    # TODO more unit tests here. Each test should test one scenario
+    # more unit tests here. Each test should test one scenario
+    def test_list_of_less_than_three_prices(self):
+        prices = [1,4]
+        self.assertEqual(0, discount(prices))
+
+    def test_list_of_one_price(self):
+        prices = [4]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_zero_price(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_in_string(self):
+        prices = ['cat', 'star', 'dog']
+        with self.assertRaises(TypeError):
+            price_discount.discount(prices)
+
+    def test_list_in_negative_numbers(self):
+        prices = [-4, -2, -10]
+        self.fail('Finish')
+
+    def test_zero_in_price_list(self):
+        prices = 0
+        self.fail('finish')
+
+
+    def test_float_point_in_price_list(self):
+        prices = [3.4, 2.6, 10.4]
+        self.fail('finish')
+
 
 
 if __name__ == '__main__':
